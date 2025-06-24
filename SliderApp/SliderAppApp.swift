c89d943ef85c7a00dea648546187e544c5276d78
gunnargray <hi@gunnargray.com>
2025-06-24 11:44:21 -0500

    Initial Commit

---
+++ SliderApp/SliderAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderAppApp.swift
+//  SliderApp
+//
+//  Created by Gunnar Gray on 6/24/25.
+//
+
+import SwiftUI
+
+@main
+struct SliderAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
